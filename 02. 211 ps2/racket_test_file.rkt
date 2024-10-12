da81bf668ac0dddb6f9d4ef99c08a63decfa2a2a
awsalmah <awsalmahasneh@gmail.com>
2024-10-12 13:28:05 -0400

    Implemented an initial version of the place_image function

--- 02. 211 ps2/racket_test_file.rkt
+++ 02. 211 ps2/racket_test_file.rkt
@@ -9,3 +9,27 @@
    (triangle 32 "solid" "red")
    0 0
    (rectangle 48 48 "solid" "gray"))
+
+(place-image
+   (triangle 32 "solid" "red")
+   24 24
+   (rectangle 48 48 "solid" "gray"))
+
+(place-image
+   (triangle 64 "solid" "red")
+   24 24
+   (rectangle 48 48 "solid" "gray"))
+
+(place-image
+   (circle 4 "solid" "white")
+   18 20
+   (place-image
+    (circle 4 "solid" "white")
+    0 6
+    (place-image
+     (circle 4 "solid" "white")
+     14 2
+     (place-image
+      (circle 4 "solid" "white")
+      8 14
+      (rectangle 24 24 "solid" "goldenrod")))))
